591f55c6347b10fd1803ee89dea5fec9f43512bd
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-08-16 17:28:22 +0500

    ✔️LEVEL 1 - TASK 1

---
+++ CALCULATOR/index.js
@@ -0,0 +1,22 @@
+let output = document.getElementById("output");
+buttons = document.querySelectorAll("button");
+let outputValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    let buttonText = e.target.innerText;
+
+    if (buttonText == "X") {
+      buttonText = "*";
+      outputValue = outputValue + buttonText;
+      output.value = outputValue;
+    } else if (buttonText == "C") {
+      outputValue = "";
+      output.value = outputValue;
+    } else if (buttonText == "=") {
+      output.value = eval(outputValue);
+    } else {
+      outputValue += buttonText;
+      output.value = outputValue;
+    }
+  });
+}
